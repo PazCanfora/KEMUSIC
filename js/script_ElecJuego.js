c3cabecf9adbb47683faf2c4c2880593cd91a417
PazCanfora <pazcanfora.et24@gmail.com>
2023-10-22 18:11:19 -0300

    Se agrega el proyecto

---
+++ js/script_ElecJuego.js
@@ -0,0 +1,22 @@
+
+let divClassic = document.getElementById("DIV_Classic");
+let divPop = document.getElementById("DIV_Pop");
+let divRock = document.getElementById("DIV_Rock");
+let divMixto = document.getElementById("DIV_Mixto");
+
+divClassic.onclick = () => {
+    window.localStorage.setItem('choose', '1');
+    window.location.href = "./JUEGOS/Classic.html";
+};
+divPop.onclick = () => {
+    window.localStorage.setItem('choose', '2');
+    window.location.href = "./JUEGOS/Pop.html";
+};
+divRock.onclick = () => {
+    window.localStorage.setItem('choose', '3');
+    window.location.href = "./JUEGOS/Rock.html";
+};
+divMixto.onclick = () => {
+    window.localStorage.setItem('choose', '4');
+    window.location.href = "./JUEGOS/Mixto.html";
+};
---
+++ js/script_Juego.js
@@ -0,0 +1,22 @@
+const GenElec = parseInt(window.localStorage.getItem("choose"));
+console.log(GenElec);
+
+let boop = new Audio();
+boop.src = "../../musica/boop.mp3";
+let bonus = new Audio();
+bonus.src = "../../musica/bonus.mp3";
+
+//--------------------------------------------------LOGICA INDIVIDUAL--------------------------------------------------
+switch (GenElec) {
+  case 1:
+    break;
+
+  case 2:
+    break;
+
+  case 3:
+    break;
+
+  case 4:
+    break;
+}
